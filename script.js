8df7f4e14670571187f4ef3809d4174bcc67b8c5
MrBurp <andreass.ziegler@gmail.com>
2022-10-19 14:07:30 +0200

    get user working

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const APIURL = "https://api.github.com/users/";
 const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
+
+getUser("Andreas-Ziegler22");
+async function getUser(username) {
+  try {
+    const { data } = await axios(APIURL + username);
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
