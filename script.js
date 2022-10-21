27c066ed99cc6ed83eb91f14e0afc3ec1d1ca082
MrBurp <andreass.ziegler@gmail.com>
2022-10-21 17:48:49 +0200

    get repos card

--- script.js
+++ script.js
@@ -9,9 +9,22 @@ async function getUser(username) {
     const { data } = await axios(APIURL + username);
 
     createUserCard(data);
+    getRepos(username);
   } catch (err) {
     if (err.response.status == 404) {
       createErrorCard("No profile with this username");
+    }
+  }
+}
+
+async function getRepos(username) {
+  try {
+    const { data } = await axios(APIURL + username + "/repos");
+
+    addReposToCard(data);
+  } catch (err) {
+    if (err.response.status == 404) {
+      createErrorCard("Problem fetching the repos");
     }
   }
 }
